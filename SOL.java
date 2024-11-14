b4b98d8eb49637b967a88dcf73ce91d0f9c7a1a5
CoffeeBeann <m261194@usna.edu>
2024-11-14 18:11:54 -0500

    Syncing SOL File

---
+++ SOL.java
@@ -0,0 +1,15 @@
+/********************************************************
+Filename: SOL.java
+Author: MIDN 2/C Coffey, MIDN 2/C Lee
+Implement a Self Organizing List Based on Access Frequency
+*********************************************************/ 
+
+// SOL Class
+public class SOL 
+{
+    // Main Method for Testing
+    public static void main(String[] args) 
+    {
+
+    }
+}
